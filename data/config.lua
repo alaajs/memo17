898595bec9a53635e11acf8c0083b298aa5ba47c
alaajs <30490322+alaajs@users.noreply.github.com>
2017-09-01 13:32:30 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,22 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "replay"
+  },
+  info_text = "  For More Information Subscribe\n\n\n\n:\n🌟| @jgjgjgjgj ➣ Developer 🌟\n🌟| https://t.me/joinchat/AAAAAEAo-QxlZdlf4x6SGw ➣ Developer Channel 🌟\n\n\n\n\n\n\n\n\n\n\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    266714448,
+    337652067
+  }
+}
+return _
+end
